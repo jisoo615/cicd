97c8cdbdb246b43dca709f54ca6c1d68608271d6
KOOHYO <108776113+KOOHYO@users.noreply.github.com>
2024-10-24 16:58:37 +0900

    build: 스프링부트 초기 셋팅 yml 설정, gitignore 설정, 기초 dependencies 추가

---
+++ src/main/java/com/haru/doyak/harudoyak/HarudoyakApplication.java
@@ -0,0 +1,13 @@
+package com.haru.doyak.harudoyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarudoyakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarudoyakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haru/doyak/harudoyak/HarudoyakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haru.doyak.harudoyak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarudoyakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
