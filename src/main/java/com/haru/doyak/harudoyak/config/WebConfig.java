f3eb32da3250956c0dc85c153c5cbcaadeefcf50
KOOHYO <108776113+KOOHYO@users.noreply.github.com>
2024-10-28 21:43:17 +0900

    build: 리액트 CORS 설정

---
+++ src/main/java/com/haru/doyak/harudoyak/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.haru.doyak.harudoyak.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    // 리액트 CORS설정
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
