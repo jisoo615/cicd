2f59e92c610fd8c6bd7285fc781a80abe361c65c
jisoo615 <57720285+jisoo615@users.noreply.github.com>
2024-11-03 22:25:28 +0900

    feat: 이메일 인증링크 전송 완료

--- src/main/java/com/haru/doyak/harudoyak/domains/auth/AuthController.java
+++ src/main/java/com/haru/doyak/harudoyak/domains/auth/AuthController.java
@@ -4,7 +4,10 @@ import com.haru.doyak.harudoyak.domains.auth.oauth.GoogleOAuthService;
 import com.haru.doyak.harudoyak.dto.auth.JoinReqDTO;
 import com.haru.doyak.harudoyak.dto.auth.LoginReqDTO;
 import com.haru.doyak.harudoyak.dto.jwt.JwtRecord;
+import jakarta.mail.MessagingException;
+import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final GoogleOAuthService oAuthService;
     private final AuthService authService;
+    private final EmailService emailService;
 
     @PostMapping("login")
     public ResponseEntity<String> login(@RequestBody LoginReqDTO loginReqDTO) throws Exception {
@@ -36,5 +40,17 @@ public class AuthController {
     public ResponseEntity<String> join(@RequestBody JoinReqDTO joinReqDto){
         authService.joinMember(joinReqDto);
         return ResponseEntity.ok().body("회원가입이 완료되었습니다.");
+    }
+
+    @PostMapping("email/verify")
+    public ResponseEntity<String> emailVerify(@RequestBody String email) throws MessagingException {
+        emailService.sendAuthLinkEmail(email);
+        return ResponseEntity.ok().body("인증 메일이 발송되었습니다.");
+    }
+
+    @GetMapping("chcek")
+    public ResponseEntity<String> chcek(@Param("nickname")String nickname){
+        // 중복확인 로직
+        return ResponseEntity.ok().body("");
     }
 }
---
+++ src/main/java/com/haru/doyak/harudoyak/domains/auth/EmailService.java
@@ -0,0 +1,36 @@
+package com.haru.doyak.harudoyak.domains.auth;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    public void sendAuthLinkEmail(String recipient) throws MessagingException {
+        String authLink = "http://localhost:5500/join?email="+recipient+"&isVerified=true&expireDate="+ LocalDateTime.now().plusDays(1);
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setTo(recipient);
+        helper.setSubject("[하루도약] 이메일 인증 요청");
+        String htmlContent = "<h1>하루도약에 오신 걸 환영합니다.</h1>" +
+                "<p>아래 링크를 클릭하여 회원가입을 완료하세요</p>" +
+                "<a href="+ authLink + ">회원가입 링크</a>" +
+                "<p>감사합니다!</p>";
+        helper.setText(htmlContent, true);
+        helper.setFrom(mailUsername);
+        javaMailSender.send(message);
+    }
+}
