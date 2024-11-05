2b81ddd98fb3c996df8777b3401ded4f133437bc
Jack Cho <ccaaca777@gmail.com>
2024-11-05 15:59:34 +0900

    fix : querydsl 설정 변경

--- src/test/java/com/haru/doyak/harudoyak/HarudoyakApplicationTests.java
+++ src/test/java/com/haru/doyak/harudoyak/HarudoyakApplicationTests.java
@@ -1,13 +1,13 @@
-package com.haru.doyak.harudoyak;
+// package com.haru.doyak.harudoyak;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HarudoyakApplicationTests {
+// @SpringBootTest
+// class HarudoyakApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
