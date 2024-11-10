9c090488b0f10c17c3232f403828e5209a9221c4
KOOHYO <108776113+KOOHYO@users.noreply.github.com>
2024-11-10 22:29:38 +0900

    fix: cors 요청 url patterns로 수정

--- src/main/java/com/haru/doyak/harudoyak/config/WebConfig.java
+++ src/main/java/com/haru/doyak/harudoyak/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("http://*:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
                 .allowCredentials(true)
