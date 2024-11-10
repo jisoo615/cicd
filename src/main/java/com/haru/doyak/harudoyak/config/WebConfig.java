694d964c78f8a697796cb5de1c94475fc6dc4796
KOOHYO <108776113+KOOHYO@users.noreply.github.com>
2024-11-10 17:57:15 +0900

    fix: cors설정 필터 설정 다시 변경

--- src/main/java/com/haru/doyak/harudoyak/config/WebConfig.java
+++ src/main/java/com/haru/doyak/harudoyak/config/WebConfig.java
@@ -1,6 +1,11 @@
 package com.haru.doyak.harudoyak.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +22,29 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .maxAge(3600);
 
+    }
+
+    // CORS 필터 등록
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("http://localhost:3000");
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("OPTIONS");
+        corsConfig.addAllowedHeader("*");  // 모든 헤더 허용
+        corsConfig.setAllowCredentials(true);  // 자격 증명 허용
+        corsConfig.setMaxAge(3600L);  // Preflight 요청에 대한 캐시 시간 설정 (1시간)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);  // 모든 엔드포인트에 CORS 설정
+
+        FilterRegistrationBean<CorsFilter> corsFilterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        corsFilterBean.addUrlPatterns("/api/*"); // CORS 필터를 적용할 URL 패턴
+        corsFilterBean.setOrder(0); // 가장 먼저 실행되도록 설정
+
+        return corsFilterBean;
     }
 
 }
