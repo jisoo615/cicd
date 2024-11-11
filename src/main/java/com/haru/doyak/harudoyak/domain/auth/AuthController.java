00e281b3ea8146596a94601694e1a80d1e0685fb
KOOHYO <108776113+KOOHYO@users.noreply.github.com>
2024-11-11 13:10:03 +0900

    fix: RequestMapping에 OPTIONS 메소드 허용 추가

--- src/main/java/com/haru/doyak/harudoyak/domain/auth/AuthController.java
+++ src/main/java/com/haru/doyak/harudoyak/domain/auth/AuthController.java
@@ -14,16 +14,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/auth")
+@RequestMapping(value = "api/auth/*", method = RequestMethod.OPTIONS)
 public class AuthController {
     private final OAuthService oAuthService;
     private final AuthService authService;
