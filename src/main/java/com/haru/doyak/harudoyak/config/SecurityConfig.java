c535d08c1a4b2fc61c62ead28f82cc4bdfbae5ea
KOOHYO <108776113+KOOHYO@users.noreply.github.com>
2024-11-12 15:06:38 +0900

    fix: 스프링 시큐리티 설정 변경

--- src/main/java/com/haru/doyak/harudoyak/config/SecurityConfig.java
+++ src/main/java/com/haru/doyak/harudoyak/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,16 +20,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .anyRequest().permitAll()  // 모든 요청에 대해 인증을 요구하지 않음
-                )
-                .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
-                .headers(headers -> headers.disable())  // X-Frame-Options 비활성화 (필요한 경우)
-                .formLogin(formLogin -> formLogin.disable())  // 기본 로그인 폼 비활성화
-                .httpBasic(httpBasic -> httpBasic.disable())  // HTTP Basic 인증 비활성화
-                .logout(logout -> logout.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
+
+//        http
+//                .authorizeRequests(authorizeRequests ->
+//                        authorizeRequests
+//                                .anyRequest().permitAll()  // 모든 요청에 대해 인증을 요구하지 않음
+//                )
+//                .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
+//                .headers(headers -> headers.disable())  // X-Frame-Options 비활성화 (필요한 경우)
+//                .formLogin(formLogin -> formLogin.disable())  // 기본 로그인 폼 비활성화
+//                .httpBasic(httpBasic -> httpBasic.disable())  // HTTP Basic 인증 비활성화
+//                .logout(logout -> logout.disable());
 
         return http.build();
     }
