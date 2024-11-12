2dce35d62ae1d65cdf7c254cd7095699565d257e
KOOHYO <108776113+KOOHYO@users.noreply.github.com>
2024-11-12 15:24:13 +0900

    fix: 스프링 시큐리티와 cors설정 합침

--- src/main/java/com/haru/doyak/harudoyak/config/SecurityConfig.java
+++ src/main/java/com/haru/doyak/harudoyak/config/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -20,19 +23,42 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
 
-//        http
-//                .authorizeRequests(authorizeRequests ->
-//                        authorizeRequests
-//                                .anyRequest().permitAll()  // 모든 요청에 대해 인증을 요구하지 않음
-//                )
-//                .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
-//                .headers(headers -> headers.disable())  // X-Frame-Options 비활성화 (필요한 경우)
-//                .formLogin(formLogin -> formLogin.disable())  // 기본 로그인 폼 비활성화
-//                .httpBasic(httpBasic -> httpBasic.disable())  // HTTP Basic 인증 비활성화
-//                .logout(logout -> logout.disable());
+        /*http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll()  // 모든 요청에 대해 인증을 요구하지 않음
+                )
+                .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
+                .headers(headers -> headers.disable())  // X-Frame-Options 비활성화 (필요한 경우)
+                .formLogin(formLogin -> formLogin.disable())  // 기본 로그인 폼 비활성화
+                .httpBasic(httpBasic -> httpBasic.disable())  // HTTP Basic 인증 비활성화
+                .logout(logout -> logout.disable());*/
 
-        return http.build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .headers(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .build();
     }
+
+    /*
+    * cors관련 설정 커스텀
+    * */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
 }
